3584e07fa3baec1338e3c815919923601a796d1d
GHenz259 <GHensley259@gmail.com>
2025-10-07 08:40:24 -0400

    10/7/2025

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -11,12 +11,19 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-
+    public GameObject projectilePrefab;
     // Update is called once per frame
     void Update()
     {
+        //Player Movement
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //Launch projectile
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
 
         // Keep the Player in bounds
 
