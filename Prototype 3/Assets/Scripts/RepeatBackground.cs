6dc94e57ece6b031a0cf073f3e98ad3efd4c7652
GHenz259 <GHensley259@gmail.com>
2025-11-03 08:36:09 -0500

    Don't Just Stand There

--- Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -5,18 +5,22 @@ public class RepeatBackground : MonoBehaviour
     private Vector3 startPos;
     private float repeatWidth;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Store the starting position of the background
         startPos = transform.position;
+
+        // Calculate the width of the background for repeating
+        // Assumes you have a BoxCollider2D or BoxCollider attached that matches the sprite size
         repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // If the background moves left past a certain point, reset its position
         if (transform.position.x < startPos.x - repeatWidth)
+        {
             transform.position = startPos;
+        }
     }
 }
