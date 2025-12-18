ab7df39b34ad1731798440901a668784a7281e4e
GHenz259 <GHensley259@gmail.com>
2025-12-18 07:57:52 -0500

    Implement data persistence between sessions

--- MSFD/Assets/Scripts/MenuUIHandler.cs
+++ MSFD/Assets/Scripts/MenuUIHandler.cs
@@ -24,6 +24,8 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -39,5 +41,20 @@ public class MenuUIHandler : MonoBehaviour
         Application.Quit();
 #endif
 
+        MainManager.Instance.SaveColor();
     }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+
+
+
 }
